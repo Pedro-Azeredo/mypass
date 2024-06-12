1f74b071e0c30cd74eb05691adfde6c0093965e8
pedro <pedroazeredo@soulasalle.com.br>
2024-06-12 05:19:18 -0300

    feature de gerar senhas implementada

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const localStorageKey = 'espaco'; //chave do local storage
 const inputRange = document.querySelector('.input-range'); // Range
 const textoInput = document.querySelector('.size'); //Tamanho
-const btnOlhoDir = document.querySelector('.espaco-olho');
+const btn = document.querySelector('.btn-gerar-senha'); //botao
 
 inputRange.addEventListener('change', () => {
   textoInput.innerHTML = +inputRange.value;
 }); //atualizar valor do tamanho da senha
-
 document.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
+    gerarSenha();
     novoEspaco();
   }
 }); // acionar quando aperto enter
-
+btn.addEventListener('click', function (e) {
+  gerarSenha();
+});
 function esconder() {
   let inputSenha = document.querySelector('.input-senha');
   if (inputSenha.type == 'password') {
@@ -21,7 +23,6 @@ function esconder() {
     inputSenha.type = 'password';
   }
 } // esconder senha box esquerda
-
 function esconderSenha() {
   let inputSenha = document.getElementsByClassName('espaco-senha-exibir');
   for (let i = 0; i < inputSenha.length; i++) {
@@ -31,18 +32,16 @@ function esconderSenha() {
       inputSenha[i].type = 'text';
     }
   }
-}
-
+} // esconder senhas no box direito
 function novoEspaco() {
+  gerarSenha();
   let inputRede = document.querySelector('.input-rede');
   let inputLogin = document.querySelector('.input-login');
   let inputSenha = document.querySelector('.input-senha');
   let contemRede = document.querySelector('.input-rede').value;
   let contemLogin = document.querySelector('.input-login').value;
-  let contemSenha = document.querySelector('.input-senha').value;
   inputRede.style.border = '';
   inputLogin.style.border = '';
-  inputSenha.style.border = '';
   //validação
   if (!inputRede.value) {
     alert('O nome da rede não pode ficar em branco');
@@ -50,10 +49,7 @@ function novoEspaco() {
   } else if (!inputLogin.value) {
     alert('O login não pode ficar em branco');
     inputLogin.style.border = '0.5px solid #8B0000';
-  } else if (!inputSenha.value) {
-    inputSenha.style.border = '0.5px solid #8B0000';
-    alert('A senha não pode ficar em branco');
-  } else if (contemLogin == "'" || contemRede == "'" || contemSenha == "'") {
+  } else if (contemLogin == "'" || contemRede == "'") {
     alert('Caracter Invalido');
   } else {
     //se der bom encrementar o local storage
@@ -73,23 +69,54 @@ function novoEspaco() {
   inputLogin.value = '';
   inputSenha.value = '';
   inputRede.value = '';
-}
+} // criar espaço para senhas
 function mostrarValues() {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
   let list = document.querySelector('.lista');
   list.innerHTML = '';
   for (let i = 0; i < values.length; i++) {
     list.innerHTML += `<li><p class="espaco-rede">${values[i]['rede']}</p>
-                      <div class="espaco"><div class="espaco-esquerda"><p class="espaco-login">Login</p><input type="text" class="espaco-login-exibir" value="${values[i]['login']}"></div><div class="espaco-direita"><p class="espaco-senha">Senha</p><input type="text" class="espaco-senha-exibir" value="${values[i]['senha']}"></div><div class="espaco-btn"><button class="espaco-olho" onclick='esconderSenha()'><img src="img/olhos.svg"></button><button onclick='removeItem("${values[i]['senha']}")'
+                      <div class="espaco"><div class="espaco-esquerda"><p class="espaco-login">Login</p><input type="text" class="espaco-login-exibir" readonly value="${values[i]['login']}"></div><div class="espaco-direita"><p class="espaco-senha">Senha</p><input type="text" class="espaco-senha-exibir" readonly value="${values[i]['senha']}"></div><div class="espaco-btn"><button class="espaco-olho" onclick='esconderSenha()'><img src="img/olhos.svg"></button><button onclick='removeItem("${values[i]['senha']}")'
                       class="espaco-lixeira"><img src="img/lixeira.svg"></button></div></div></li>`;
   }
-}
+} // atualiza a tela
 function removeItem(data) {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
   let index = values.findIndex((x) => x.senha == data);
   values.splice(index, 1);
   localStorage.setItem(localStorageKey, JSON.stringify(values));
   mostrarValues();
-}
+} // remove um item
+function embaralhar() {
+  const letras =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  const caractere = letras.split('');
+
+  for (let i = 0; i < caractere.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [caractere[i], caractere[j]] = [caractere[j], caractere[i]];
+  }
+  return caractere.join('');
+} // embaralha senha
+function tamanho(senha, novoTamanho) {
+  let senhaArray = senha.split('');
+
+  let novaSenha = [];
+  for (let i = 0; i < novoTamanho && i < senhaArray.length; i++) {
+    novaSenha[i] = senhaArray[i];
+  }
+
+  novaSenha = novaSenha.join('');
+
+  return novaSenha;
+} // carrega a senha com o tamanho definido
+function gerarSenha() {
+  const inputSenha = document.querySelector('.input-senha');
+  let senha = embaralhar();
+  let senhaPronta = tamanho(senha, inputRange.value);
+  inputSenha.value = senhaPronta;
+} // ingere a senha no input
+
 //execução automatica quando inicia a pagina
+gerarSenha();
 mostrarValues();
